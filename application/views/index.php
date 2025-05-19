5991ab1503136910e01d47148c87d783dda24aae
Ruan <ruannandes4567@gmail.com>
2025-05-19 19:23:50 -0300

    ícone colocado

--- application/views/index.php
+++ application/views/index.php
@@ -2,16 +2,14 @@
 <html>
 <head>
 	<title>G-Max</title>
-	<link rel="shortcut icon" href="assets/icon_g_max.jpeg" type="image/x-icon">
-	<link rel="shortcut icon" href="" type="image/x-icon">
+	<link rel="icon" href="<?php echo base_url('assets/icon_Gmax.ico?'); ?>" type="image/x-icon">
 </head>
 <body>
 
-<header id='mainHeader'>
-	<nav id= 'mainNav'>
-		
-	</nav>
-</header>
-
+	<header id='mainHeader'>
+		<nav id= 'mainNav'>
+			<img src="<?php echo base_url('assets/icon_Gmax.jpg')?>" alt="Imagem">
+		</nav>
+	</header>
 </body>
 </html>
