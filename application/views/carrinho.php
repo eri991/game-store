01abbdf3a6df52bc1b16fa3aed242bf3b9498daf
Eric Barbosa <ericbarbosa4050@gmail.com>
2025-05-23 22:50:26 -0300

    Adicionando pagina do carrinho

--- application/views/carrinho.php
+++ application/views/carrinho.php
@@ -2,23 +2,94 @@
 <html>
 <head>
 	<title>G-Max</title>
-	<!-- <link rel="icon" href="<?php echo base_url('assets/icon_Gmax.ico'); ?>" type="image/x-icon"> -->
-	<link rel="shortcut icon" href="/game-store/assets/icon_Gmax.ico" type="image/x-icon">
-	<link rel="stylesheet" href="/game-store/assets/loginPage.css">
+	<link rel="icon" href="<?php echo base_url('assets/icon_Gmax.ico'); ?>" type="image/x-icon">
+	<link rel="shortcut icon" href="<?php echo base_url('assets/icon_Gmax.ico'); ?>" type="image/x-icon">
+	<link rel="stylesheet" href="<?php echo base_url('assets/carrinhoPage.css'); ?>">
 </head>
 <body>
 
 	<header id='mainHeader'>
 		<nav id= 'mainNav'>
 			<div>
-				<img id= 'logoHeader' src="/game-store/assets/icon_Gmax.ico" alt="Logo">
-				<!-- <img id= 'logoHeader' src="<?php echo base_url('assets/icon_Gmax.jpg')?>" alt="Logo"> -->
+				<img id= 'logoHeader' src="<?php echo base_url('assets/icon_Gmax.jpg')?>" alt="Logo">
 				<p>Cadastrar</p>
 			</div>
 			<a href="https://help.steampowered.com/pt/" target="_blank">Precisa de ajuda?</a>
             <a href="<?= base_url('carrinho') ?>">Carrinho</a>
 		</nav>
 	</header>
+	<main class='mainCarrinho'>
+		<div class="listaProdutos">
+			<div class="linhaProduto">
+				<div class="wrapProduto">
+					<div class="qntProduto">
+						<button class="aumentarQnt">+</button>
+						<p class="qntSelecionada">1</p>
+						<button class="diminuirQnt">-</button>
+					</div>
+					<div class="imgLinhaProduto">
+
+					</div>
+					<div class="infosProduto">
+						<p class='tituloProduto'>Título</p>
+						<p class="descProduto">Descrição. Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+						<p class="precoProduto">R$ 105,87</p>
+					</div>
+				</div>
+			</div>
+			<div class="linhaProduto">
+				<div class="wrapProduto">
+					<div class="qntProduto">
+						<button class="aumentarQnt">+</button>
+						<p class="qntSelecionada">1</p>
+						<button class="diminuirQnt">-</button>
+					</div>
+					<div class="imgLinhaProduto">
+
+					</div>
+					<div class="infosProduto">
+						<p class='tituloProduto'>Título</p>
+						<p class="descProduto">Descrição. Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+						<p class="precoProduto">R$ 85,90</p>
+					</div>
+				</div>
+			</div>
+			
+		</div>
+		<div class="cardPrecos">
+			<div class="cabecalhoResumo">
+				<h3>Resumo</h3>
+				<div class="imagensProdutos">
+					<div class="imgResumo">
+
+					</div>
+					<div class="imgResumo">
+
+					</div>
+				</div>
+			</div>
+			
+			<div class="listaPrecos">
+				<div class="linhaPreco">
+					<p class="tituloPreco">Título</p>
+					<p class="valorPreco">R$ 105,87</p>
+				</div>
+				<div class="linhaPreco">
+					<p class="tituloPreco">Título</p>
+					<p class="valorPreco">R$ 85,90</p>
+				</div>
+			</div>
+			<div class="total">
+				<div class="valorTotal">
+					<p class="tituloTotal">Preço Total</p>
+					<p class="valorTotal">R$ 191,77</p>
+				</div>
+				<button class="continuarCompra">
+					Continuar
+				</button>
+			</div>
+		</div>
+	</main>
     
 </body>
 </html>
