0a2686971122ff508f1731439eb6781923692afe
Ruan <ruannandes4567@gmail.com>
2025-05-27 09:21:21 -0300

    alterações no carrinho

--- application/views/carrinho.php
+++ application/views/carrinho.php
@@ -5,14 +5,15 @@
     <link rel="icon" href="<?php echo base_url('assets/img/icon_Gmax.ico'); ?>" type="image/x-icon">
     <link rel="shortcut icon" href="<?php echo base_url('assets/img/icon_Gmax.ico'); ?>" type="image/x-icon">
     <link rel="stylesheet" href="<?php echo base_url('assets/css/carrinhoPage.css'); ?>?v=<?= time() ?>">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
 
 <header id='mainHeader'>
     <nav id='mainNav'>
         <div>
-            <img id='logoHeader' src="<?php echo base_url('assets/img/icon_Gmax.jpg') ?>" alt="Logo">
-            <p>Cadastrar</p>
+            <img id='logoHeader' src="<?php echo base_url('assets/img/icon_Gmax.ico') ?>" alt="Logo G-Max">
+            <p>Carrinho</p>
         </div>
         <a href="https://help.steampowered.com/pt/" target="_blank">Precisa de ajuda?</a>
         <a href="<?= base_url('carrinho') ?>">Carrinho</a>
@@ -25,7 +26,7 @@
         <div class="linhaProduto">
             <div class="wrapProduto">
                 <div class="qntProduto">
-                    <button class="aumentarQnt">+</button>
+                    <button class="aumentarQnt"><i class="fa-solid fa-plus"></i></button>
                     <p class="qntSelecionada"><?= $item->quantidade ?></p>
                     <button class="diminuirQnt">-</button>
                 </div>
