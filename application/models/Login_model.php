7c2fe82ab24c3fe610f94dc6efd920cd2a5243c1
Eric Barbosa <ericbarbosa4050@gmail.com>
2025-06-19 14:57:31 -0300

    Resolvendo erro get usuarios

--- application/models/Login_model.php
+++ application/models/Login_model.php
@@ -1,18 +1,17 @@
 <?php
 Class Login_model extends CI_Model{
     function buscar_nickname($nickname){
-        $query = $this->db->get_where('usuario',['nickname' => $nickname]);
+        $query = $this->db->get_where('usuarios',['nickname' => $nickname]);
         return $query->num_rows() > 0;
     }
     function buscar_senha($nickname,$senha){
-        $query = $this->db->get_where('usuario',['nickname' => $nickname]);
+        $query = $this->db->get_where('usuarios',['nickname' => $nickname]);
         if($query->num_rows() > 0){
             if($query->row()->senha == $senha){
                 return true;
             }else{
                 return false;
             }
-            return false;
         }
 
     }
